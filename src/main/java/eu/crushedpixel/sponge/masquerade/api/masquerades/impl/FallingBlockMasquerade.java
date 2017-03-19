2a6356819e648b4255023e99ffc6741144169e8c
CrushedPixel <crushedpixelmaps@gmail.com>
2017-03-19 23:38:45 +0100

    FallingBlockMasquerade accepts a BlockType instead of block id to allow for better accessibility

--- src/main/java/eu/crushedpixel/sponge/masquerade/api/masquerades/impl/FallingBlockMasquerade.java
+++ src/main/java/eu/crushedpixel/sponge/masquerade/api/masquerades/impl/FallingBlockMasquerade.java
@@ -1,15 +1,17 @@
 package eu.crushedpixel.sponge.masquerade.api.masquerades.impl;
 
 import eu.crushedpixel.sponge.masquerade.api.masquerades.ObjectMasquerade;
+import net.minecraft.block.Block;
 import net.minecraft.entity.item.EntityFallingBlock;
+import org.spongepowered.api.block.BlockType;
 import org.spongepowered.api.entity.living.player.Player;
 
 public class FallingBlockMasquerade extends ObjectMasquerade<EntityFallingBlock> {
 
     private static final int FALLING_BLOCK_ID = 70;
 
-    public FallingBlockMasquerade(Player player, int blockID, int metadata) {
-        super(player, EntityFallingBlock.class, FALLING_BLOCK_ID, blockID | (metadata << 12));
+    public FallingBlockMasquerade(Player player, BlockType blockType, int metadata) {
+        super(player, EntityFallingBlock.class, FALLING_BLOCK_ID, Block.getIdFromBlock((Block) blockType) | (metadata << 12));
     }
 
 }
