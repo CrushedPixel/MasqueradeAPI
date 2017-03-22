61e01ce88689805629ee2da6466ebec0bc8f0cd7
CrushedPixel <crushedpixelmaps@gmail.com>
2017-03-22 13:02:02 +0100

    Add javadoc Swap Masquerades#fallingBlock arguments for consistency

--- src/main/java/eu/crushedpixel/sponge/masquerade/api/Masquerade.java
+++ src/main/java/eu/crushedpixel/sponge/masquerade/api/Masquerade.java
@@ -9,12 +9,14 @@ import java.util.Collection;
 public interface Masquerade {
 
     /**
-     * Shows the masquerade to the specific player.
+     * Shows the masquerade to a player.
+     * @param player The player to show the masquerade to
      */
     void maskTo(Player player);
 
     /**
-     * Removes the masquerade for the specified player.
+     * Removes the masquerade for a player.
+     * @param player The player to remove the masquerade for
      */
     void unmaskTo(Player player);
 
@@ -23,16 +25,49 @@ public interface Masquerade {
      */
     void unmask();
 
+    /**
+     * Modifies this masquerade's data.
+     * @param key The data key to modify
+     * @param value The value to set
+     * @param <V> The data type
+     * @throws IllegalArgumentException if the key is not supported by this masquerade
+     */
     <V> void setData(Key<? extends BaseValue<V>> key, V value);
 
+    /**
+     * Returns this masquerade's value for a data key.
+     * @param key The data key to retrieve the value for
+     * @param <V> The data type
+     * @return The value
+     * @throws IllegalArgumentException if the key is not supported by this masquerade
+     */
     <V> V getData(Key<? extends BaseValue<V>> key);
 
-    <V> boolean hasKey(Key<? extends BaseValue<V>> key);
+    /**
+     * Returns whether a data key is supported by this masquerade.
+     * @param key The key in question
+     * @return {@code true} if supported, {@code false} otherwise
+     */
+    boolean hasKey(Key<?> key);
 
+    /**
+     * Returns a collection of all data keys supported by this masquerade.
+     * @return Collection of keys
+     */
     Collection<Key<?>> getKeys();
 
-    void setAllowValueChange(Key key, boolean allowValueChange);
+    /**
+     * Defines whether the server is allowed to modify a key by itself.
+     * @param key The key in question
+     * @param valueChangeAllowed {@code true} if allowed, {@code false} otherwise
+     */
+    void setValueChangeAllowed(Key<?> key, boolean valueChangeAllowed);
 
-    boolean allowsValueChange(Key key);
+    /**
+     * Returns whether the server is allowed to modify a key by itself.
+     * @param key The key in question
+     * @return {@code true} if allowed, {@code false} otherwise
+     */
+    boolean isValueChangeAllowed(Key<?> key);
 
 }
--- src/main/java/eu/crushedpixel/sponge/masquerade/api/Masquerades.java
+++ src/main/java/eu/crushedpixel/sponge/masquerade/api/Masquerades.java
@@ -8,10 +8,29 @@ import java.util.function.Function;
 
 public interface Masquerades {
 
+    /**
+     * Registers a function creating a {@link Masquerade}
+     * to be applied by {@link Masquerades#fromType(EntityType, Player)}.
+     * @param entityType The masquerade's entity type
+     * @param function The function returning a masquerade,
+     *                 masking the player for that entity type
+     */
     void registerDefault(EntityType entityType, Function<Player, Masquerade> function);
 
+    /**
+     * Creates a masquerade from an {@link EntityType}.
+     * @param entityType The masquerade's entity type
+     * @param player The player to be masked
+     * @return The masquerade
+     */
     Masquerade fromType(EntityType entityType, Player player);
 
-    Masquerade fallingBlock(Player player, BlockState blockState);
+    /**
+     * Creates a FallingBlock masquerade from a {@link BlockState}.
+     * @param blockState The appearance of the masquerade
+     * @param player The player to be masked
+     * @return The masquerade
+     */
+    Masquerade fallingBlock(BlockState blockState, Player player);
 
 }
